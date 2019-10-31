ac4000a620414cd2acb7b829b127b43063c988f5
Errol Roberts <errol.me@gmaill.com>
2019-10-31 17:22:18 -0400

    Removed emplid and all references

--- src/CUNY.java
+++ src/CUNY.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 public class CUNY {
-    private String emplID;
     private String name;
     private String campus;
     private int age;
@@ -15,26 +14,12 @@ public class CUNY {
             System.out.println("Invalid, age please enter a valid age between 15 and 100");
             while(age < 15 || age > 100){
                 Scanner x = new Scanner(System.in);
+                x.close();
                 int i = x.nextInt();
                 age = i;
 
             }
             this.age = age;
-        }
-    }
-
-    public String getEmplID() {
-        return emplID;
-    }
-
-    public void setEmplID(String emplID) {
-        if(emplID.length() < 8 || emplID.length() > 8)
-        {
-            System.out.println("Invalid id format too short or too long");
-            this.emplID = null;
-        }
-        else {
-            this.emplID = emplID;
         }
     }
 
@@ -54,20 +39,10 @@ public class CUNY {
         this.campus = campus;
     }
     public CUNY(){
-        emplID = "00000000";
         name = "John Doe";
         campus = "Unknown";
     }
-    public CUNY(String emplID, String name, String campus){
-        if (emplID.length() < 8 || emplID.length() > 8)
-        {
-            System.out.println("Invalid emplID");
-            this.emplID = null;
-        }
-        else
-        {
-            this.emplID = emplID;
-        }
+    public CUNY(String name, String campus){
         this.name = name;
         this.campus = campus;
     }
