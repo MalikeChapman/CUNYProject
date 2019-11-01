7b2c53f308e8b2e4d1172fb0ca9d522e3ff7fb0d
Asher Chapman <ashchapman330@gmail.com>
2019-11-01 17:32:18 -0400

    Added a test password and email validator using regex

--- src/QueensStudent.java
+++ src/QueensStudent.java
@@ -1,3 +1,4 @@
+import java.util.regex.*;
 public class QueensStudent extends CUNY {
     private String semesterEntered;
     private String semester;
@@ -7,8 +8,41 @@ public class QueensStudent extends CUNY {
     private String allClasses;
     private String password;
     private int studentID;
+    private String emailRegEx;
+    private String email;
+    private String passwordRegEX;
 
+    public String getPasswordRegEX() {
+        return passwordRegEX;
+    }
 
+    public void setPasswordRegEX() {
+        this.passwordRegEX = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^\\w\\s]).{5,10})";
+    }
+    public boolean isValidPassword( String password){
+        Pattern pass = Pattern.compile(this.passwordRegEX);
+        if (password == null)
+            return false;
+         return pass.matcher(password).matches();
+    }
+
+    public String getEmailRegEx() {
+        return emailRegEx;
+    }
+
+    public void setEmailRegEx() {
+        this.emailRegEx = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                            "[a-zA-Z0-9_+&*-]+)*@" +
+                            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                            "A-Z]{2,7}$";
+    }
+    public QueensStudent(){
+        this.emailRegEx = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+    }
 
     public String getSemesterEntered() {
         return semesterEntered;
@@ -60,6 +94,46 @@ public class QueensStudent extends CUNY {
 
     public void setMajor(String major) {
         this.major = major;
+    }
+    public boolean isValidEmail(String emailRegEx, String email){
+        Pattern p = Pattern.compile(emailRegEx);
+        if (email == null)
+            return false;
+        return p.matcher(email).matches();
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public static void main(String[] args){
+        QueensStudent q1 = new QueensStudent();
+        QueensStudent q2 = new QueensStudent();
+        q1.setEmail("Test.email@test.org");
+        q2.setEmail("test..23@..com");
+        System.out.println(q1.isValidEmail(q1.emailRegEx, q1.email));
+        System.out.println(q2.isValidEmail(q2.emailRegEx, q2.email));
+        q1.setPasswordRegEX();
+        q2.setPasswordRegEX();
+        q1.setPassword("ab12d5");
+        q2.setPassword("Test@11");
+        System.out.println(q1.isValidPassword(q1.password));
+        System.out.println(q2.isValidPassword(q2.password));
+
+
     }
 
 
